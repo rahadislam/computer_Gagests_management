81316e992fb4497de71a33c4965bed9f82a6964a
rahadislam <rahadislm.web@gmil.com>
2022-05-23 00:29:05 +0600

    testing client site

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './shaerd/Header/Header';
+import  Home  from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+    <div className="hero min-h-screen">
+    <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src="https://img.freepik.com/free-vector/virtual-influencer-abstract-concept-vector-illustration-influencer-marketing-digital-agency-service-virtual-character-computer-generated-person-social-media-brand-avatar-abstract-metaphor_335657-4171.jpg?w=2000" className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        </div>
+    </div>
+</div>
+    );
+};
+
+export default Home;
