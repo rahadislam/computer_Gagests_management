b0f6ad4b36bb6d5e931a1c0b4387c2622e44c361
rahadislam <rahadislm.web@gmil.com>
2022-05-25 15:45:02 +0600

    Delete product implement

--- src/pages/Dashbord/Allitem.js
+++ src/pages/Dashbord/Allitem.js
@@ -1,10 +1,84 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 
 const Allitem = () => {
+
+
+    const { data: service, isLoading,refetch } = useQuery('service', () => fetch('http://localhost:5000/service').then(res => res.json()));
+    if (isLoading) {
+        return <p>Loading.......</p>
+    }
+
+    const handelDelete=(_id)=>{
+        
+        fetch(`http://localhost:5000/service/${_id}`,{
+            method: 'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if (data.deletedCount) {
+                toast.success(` is deleted.`)
+                refetch();
+        }
+    })
+
+    }
     return (
-        <div>
-            
-        </div>
+        <div class="overflow-x-auto w-full">
+  <table class="table w-full">
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" class="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Avilable</th>
+        <th>Price</th>
+        <th>Minimum</th>
+        <th>Details</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            service.map(s=><tr>
+                <th>
+                  <label>
+                    <input type="checkbox" class="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div class="flex items-center space-x-3">
+                    <div class="avatar">
+                      <div class="mask mask-squircle w-12 h-12">
+                        <img src={s.img} alt='' />
+                      </div>
+                    </div>
+                    <div>
+                      <div class="font-bold">{s.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {s.Quantity}
+                </td>
+                <td>$ {s.Price}</td>
+                <td>{s.minquantity}</td>
+                <th>
+                  <button onClick={()=>handelDelete(s._id)} class="btn btn-sm bg-red-600 border-none text-white">Delete</button>
+                </th>
+              </tr>)
+        }
+      
+      
+      
+      
+    </tbody>
+    
+  </table>
+</div>
     );
 };
 
--- src/pages/Home/Card.js
+++ src/pages/Home/Card.js
@@ -5,7 +5,7 @@ const Card = ({cart}) => {
     return (
             <div class="card w-80 bg-base-100 shadow-xl">
                 <figure class="px-10 pt-10">
-                    <img src={cart.img} alt="Shoes" class="rounded-xl" />
+                    <img src={cart.img} alt="Shoes" class="rounded-xl h-40 w-60" />
                 </figure>
                 <div class="card-body items-center text-center">
                     <h2 class="card-title">{cart.name}</h2>
